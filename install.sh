03c804a211723c971c01f95ab7e69237fb6c7732
owpk <53643556+owpk@users.noreply.github.com>
2024-08-20 22:59:31 +0800

    MAIN: useful scripts

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir $HOME/.config
+mkdir $HOME/.local/bin
+CFG=$HOME/.config
+CUR=$(pwd)
+
+ln -s $CUR/.config/zsh $CFG/
+ln -s $CUR/.config/nvim $CFG/
+ln -s $CUR/.zshenv $HOME/
+ln -s $CUR/.tmux.conf $HOME/
+cp scripts/* $HOME/.local/bin
