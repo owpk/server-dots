49560374582fbf47234a8378856b2317c357f68e
owpk <53643556+owpk@users.noreply.github.com>
2024-08-24 02:21:37 +0800

    2024-08-24-updates

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "please make sure you have install dependencies: $(cat ./deps)"
 LOCAL_BIN=$HOME/.local/bin
 CFG=$HOME/.config
 
-mkdir $CFG 2> /dev/null
+mkdir $CFG 4> /dev/null
 mkdir $LOCAL_BIN 2> /dev/null
 
 CUR=$(pwd)
@@ -17,3 +17,9 @@ ln -nsf $CUR/.zshenv $HOME/
 ln -nsf $CUR/.tmux.conf $HOME/
 
 git checkout -b $USER
+
+read -p "Install gigachat? (Y/n): " INSTALL_GIGA
+
+if [ "$INSTALL_GIGA" == "Y" ]; then
+    sh <(curl -L https://github.com/owpk/gigachat-grpc-client/blob/main/install.sh)
+fi
