9a6322b47c201f58fd4ac5c015547086ddd8be09
owpk <53643556+owpk@users.noreply.github.com>
2024-08-23 22:27:08 +0800

    2024-08-23-updates

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-./install-wo-githooks.sh
+echo "please make sure you have install dependencies: $(cat ./deps)"
+
+LOCAL_BIN=$HOME/.local/bin
+CFG=$HOME/.config
+
+mkdir $CFG 2> /dev/null
+mkdir $LOCAL_BIN 2> /dev/null
+
+CUR=$(pwd)
+
+stow --adopt -vt $CFG .config
+stow --adopt -vt $LOCAL_BIN scripts 
+
+ln -nsf $CUR/.zshenv $HOME/
+ln -nsf $CUR/.tmux.conf $HOME/
+
 git checkout -b $USER
