8c70e16b86422b932c6f3c23f66f6e32926e82b8
slavavorobev <--auto-->
2024-08-28 19:38:46 +0800

    JDTSL: update

--- .config/nvim/lua/owpk/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/owpk/plugins/lsp/lspconfig.lua
@@ -2,7 +2,7 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-      "mfussenegger/nvim-jdtls", -- java support
+		"mfussenegger/nvim-jdtls", -- java support
 		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		{ "folke/neodev.nvim", opts = {} },
@@ -106,6 +106,150 @@ return {
 								callSnippets = "Replace",
 							},
 						},
+					},
+				})
+			end,
+
+			["jdtls"] = function()
+				local home = os.getenv("HOME")
+				if vim.fn.has("mac") == 1 then
+					WORKSPACE_PATH = home .. "/workspace/"
+					CONFIG = "mac"
+				elseif vim.fn.has("unix") == 1 then
+					WORKSPACE_PATH = home .. "/workspace/"
+					CONFIG = "linux"
+				else
+					print("Unsupported system")
+				end
+
+				local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+				local workspace_dir = WORKSPACE_PATH .. project_name
+
+				lspcfg["jdtls"].setup({
+					cmd = {
+
+						-- 💀
+						"java", -- or '/path/to/java11_or_newer/bin/java'
+						-- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+						"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+						"-Dosgi.bundles.defaultStartLevel=4",
+						"-Declipse.product=org.eclipse.jdt.ls.core.product",
+						"-Dlog.protocol=true",
+						"-Dlog.level=ALL",
+						"-javaagent:" .. home .. "/.local/share/nvim/mason/share/jdtls/lombok.jar",
+						"-Xms1g",
+						"--add-modules=jdk.incubator.vector",
+						"--add-modules=ALL-SYSTEM",
+						"--add-opens",
+						"java.base/java.util=ALL-UNNAMED",
+						"--add-opens",
+						"java.base/java.lang=ALL-UNNAMED",
+
+						-- 💀
+						"-jar",
+						vim.fn.glob(
+							home .. "/.local/share/nvim/mason/share/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"
+						),
+						-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+						-- Must point to the                                                     Change this to
+						-- eclipse.jdt.ls installation                                           the actual version
+
+						-- 💀
+						"-configuration",
+						home .. "/.local/share/nvim/mason/share/jdtls/config_" .. CONFIG,
+						-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+						-- Must point to the                      Change to one of `linux`, `win` or `mac`
+						-- eclipse.jdt.ls installation            Depending on your system.
+
+						-- 💀
+						-- See `data directory configuration` section in the README
+						"-data",
+						workspace_dir,
+					},
+
+					--on_attach = require("user.lsp.handlers").on_attach,
+					capabilities = capabilities,
+
+					-- 💀
+					-- This is the default if not provided, you can remove it. Or adjust as needed.
+					-- One dedicated LSP server & client will be started per unique root_dir
+					root_dir = function()
+						vim.fs.root(0, { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" })
+					end,
+
+					-- Here you can configure eclipse.jdt.ls specific settings
+					-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+					-- or https://github.com/redhat-developer/vscode-java#supported-vs-code-settings
+					-- for a list of options
+					settings = {
+						java = {
+							-- jdt = {
+							--   ls = {
+							--     vmargs = "-XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Dsun.zip.disableMemoryMapping=true -Xmx1G -Xms100m"
+							--   }
+							-- },
+							eclipse = {
+								downloadSources = true,
+							},
+							configuration = {
+								updateBuildConfiguration = "interactive",
+							},
+							maven = {
+								downloadSources = true,
+							},
+							implementationsCodeLens = {
+								enabled = true,
+							},
+							referencesCodeLens = {
+								enabled = true,
+							},
+							references = {
+								includeDecompiledSources = true,
+							},
+							inlayHints = {
+								parameterNames = {
+									enabled = "all", -- literals, all, none
+								},
+							},
+							format = {
+								enabled = false,
+							},
+						},
+						signatureHelp = { enabled = true },
+						completion = {
+							favoriteStaticMembers = {
+								"org.hamcrest.MatcherAssert.assertThat",
+								"org.hamcrest.Matchers.*",
+								"org.hamcrest.CoreMatchers.*",
+								"org.junit.jupiter.api.Assertions.*",
+								"java.util.Objects.requireNonNull",
+								"java.util.Objects.requireNonNullElse",
+								"org.mockito.Mockito.*",
+							},
+						},
+						contentProvider = { preferred = "fernflower" },
+						--extendedClientCapabilities = extendedClientCapabilities,
+						sources = {
+							organizeImports = {
+								starThreshold = 9999,
+								staticStarThreshold = 9999,
+							},
+						},
+						codeGeneration = {
+							toString = {
+								template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+							},
+							useBlocks = true,
+						},
+					},
+
+					flags = {
+						allow_incremental_sync = true,
+					},
+
+					init_options = {
+						bundles = {},
 					},
 				})
 			end,
